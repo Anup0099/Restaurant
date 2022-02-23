912475be6f77db3d4ee34c5e147b2dea2f9862c9
anup <anuppal602@gmail.com>
2022-02-23 16:28:29 +0530

    first

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(true);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app__logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          {" "}
+          <a href="#home">home</a>
+        </li>
+        <li className="p__opensans">
+          {" "}
+          <a href="#about">about</a>
+        </li>
+        <li className="p__opensans">
+          {" "}
+          <a href="#menu">menu</a>
+        </li>
+        <li className="p__opensans">
+          {" "}
+          <a href="#awards">awards</a>
+        </li>
+        <li className="p__opensans">
+          {" "}
+          <a href="#contact">contacts</a>
+        </li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Login/Registration
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          book table
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#menu">Menu</a>
+              </li>
+              <li>
+                <a href="#awards">Awards</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
