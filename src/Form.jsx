a3f795a8915d927e3a35ad628b957efdfa3088f8
anup <anuppal602@gmail.com>
2022-03-11 15:04:20 +0530

    specialmenu

---
+++ src/Form.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+  render() {
+    return (
+      <form>
+      <div>
+      <label >username</label>
+      <input type="text" />
+      </div>
+      </form>
+    )
+  }
+}
+
+export default Form
