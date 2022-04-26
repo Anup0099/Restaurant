921461868088292fcd89f2f0cd24849e0f368803
anup <anuppal602@gmail.com>
2022-04-26 19:36:32 +0530

    img

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import { images, data } from "../../constants";
+import { SubHeading, MenuItem } from "../../components";
 const FindUs = () => (
   <div>
-    FindUs
+      <div className="app__bg app__wrapper section__padding" id="contact">
+        <div className="app__wrapper_info">
+  <SubHeading title="Contact"/>
+        </div>
+        <div className="app__wrapper _img">
+          <img src={images.findus} alt="findus" />
+        </div>
+
+      </div>
   </div>
 );
 
