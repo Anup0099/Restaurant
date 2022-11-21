d8b9964e1e5e8accd48f2c8f01740cd53fdfcfbe
anup <anuppal602@gmail.com>
2022-11-21 23:02:41 +0530

    done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import {images} from '../../constants'
-import './Header.css';
-import {SubHeading} from "../../components";
+import React from 'react'
+import { images } from '../../constants'
+import './Header.css'
+import { SubHeading } from '../../components'
 
 const Header = () => (
-  <div className=" app__header app__wrapper section__padding" id='home'>
-    <div className='app__wrapper_info'>
+  <div className=" app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
       <SubHeading title="Choose the new flavour" />
-        <h1 className='app__header-h1'>key to fine dining </h1>
-        <p className='app__wrapper_img' style={{}}>
-            anna kabhi is haeli me kaat k kha jaunga  dvdkv
-        </p>
-            <button className='custom__button'>Explore menu</button>
+      <h1 className="app__header-h1">key to fine dining </h1>
+
+      <button className="custom__button">Explore menu</button>
     </div>
     <div className="app__wrapper_img">
-    <img src={images.welcome} alt="header img"/>
+      <img src={images.welcome} alt="header img" />
     </div>
-
   </div>
-);
+)
 
-export default Header;
+export default Header
